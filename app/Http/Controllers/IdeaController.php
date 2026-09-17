dccd223626e7147a902dbd910b8d688e98d7234d
Martin Kilp <martin.kilp@tptlive.ee>
2026-09-17 22:39:06 +0300

    Request Validation

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -31,6 +31,12 @@ class IdeaController extends Controller
      */
     public function store(Request $request)
     {
+        request()->validate([
+            'description' => ['required', 'min:10'],
+        ]);
+
+        
+
         Idea::create([
             'description' => request('description'),
             'state' => 'pending',
