365d56ddd9f5274315aaf440da0e4dcdb0a3ee55
Martin Kilp <martin.kilp@tptlive.ee>
2026-09-17 23:01:18 +0300

    Form Request Classes

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreIdeaRequest;
 use App\Models\Idea;
 use Illuminate\Http\Request;
 
@@ -29,11 +30,13 @@ class IdeaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreIdeaRequest $request)
     {
-        request()->validate([
-            'description' => ['required', 'min:10'],
-        ]);
+
+
+        // request()->validate([
+        //     'description' => ['required', 'min:10'],
+        // ]);
 
         
 
